b4965cf5828eb8fcf43290f66e07ab25675df741
Thibault CHEVALLERAUD <thibault.chevalleraud@gmail.com>
2022-02-02 18:56:42 +0100

    first commit

---
+++ XIQSEFactory.php
@@ -0,0 +1,14 @@
+<?php
+namespace Tchevalleraud\ExtremeNetworks\XIQSE;
+
+final class XIQSEFactory {
+
+    public function __construct(){
+
+    }
+
+    public function getTest(){
+        return "ok";
+    }
+
+}
